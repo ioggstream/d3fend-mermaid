af0fb07ef0ce1d39bbce34e2a7c306df8d657efc
Roberto Polli <robipolli@gmail.com>
2024-02-02 15:35:51 +0100

    Fix:lint

--- packages/mermaid/src/diagrams/d3fend/d3fend.js
+++ packages/mermaid/src/diagrams/d3fend/d3fend.js
@@ -1,7 +1,6 @@
 import { log } from '../../logger.js';
 import { D3FEND_DATA } from './d3fendData.js';
 
-
 const ICON_MAP = {
   'd3f:EncryptionAlgorithm': '\\ud83d\\udd10',
   'd3f:ApplicationLayerFirewall': '\\ud83d\\udee1',
@@ -78,26 +77,34 @@ const ICON_MAP = {
   'd3f:WirelessRouter': 'fa:fa-wifi',
 };
 
-
+/**
+ *
+ * @param str
+ * @param position
+ * @param searchValue
+ * @param replaceValue
+ */
 function replaceAfterPosition(str, position, searchValue, replaceValue) {
-    let firstPart = str.substring(0, position);
-    let secondPart = str.substring(position);
-    secondPart = secondPart.replace(searchValue, replaceValue);
-    return firstPart + secondPart;
+  let firstPart = str.substring(0, position);
+  let secondPart = str.substring(position);
+  secondPart = secondPart.replace(searchValue, replaceValue);
+  return firstPart + secondPart;
 }
 
+/**
+ *
+ * @param needle
+ */
 function createTooltip(needle) {
   const icon = ICON_MAP[needle] || needle;
   let url = ''; // default value
-  if (D3FEND_DATA["D3F_DIGITAL_ARTIFACTS"].includes(needle)) {
+  if (D3FEND_DATA['D3F_DIGITAL_ARTIFACTS'].includes(needle)) {
     url = `https://next.d3fend.mitre.org/dao/artifact/${needle}/`;
-  }
-  else if (D3FEND_DATA["D3F_DEFENSIVE_TECHNIQUES"].includes(needle)) {
+  } else if (D3FEND_DATA['D3F_DEFENSIVE_TECHNIQUES'].includes(needle)) {
     url = `https://next.d3fend.mitre.org/dao/technique/${needle}/`;
   }
   return `<a title='${needle}' href='${url}' target='_blank' rel='noopener noreferrer'>${icon}</a>`;
 }
-
 
 export const renderD3fendIcons = function (text) {
   let matches = text.match(/d3f:\w+/g);
@@ -106,8 +113,7 @@ export const renderD3fendIcons = function (text) {
   if (matches) {
     matches.forEach((needle) => {
       let replacement = createTooltip(needle);
-      text = replaceAfterPosition(text, position,
-        needle, replacement);
+      text = replaceAfterPosition(text, position, needle, replacement);
       position += replacement.length - needle.length;
       log.debug('found', needle, text);
     });
